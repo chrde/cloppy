1d5df8940108510fa67a11f0404d330ff2c674b3
Christian de la Hoz <apigolas@gmail.com>
2018-03-26 21:47:36 +0200

    async_consumer is not needed

--- src/parse_mft.rs
+++ src/parse_mft.rs
@@ -4,9 +4,7 @@ use windows::async_io::{
     BufferPool,
     AsyncReader,
     IOCompletionPort,
-    AsyncConsumer,
     OutputOperation,
-    Consumer,
 };
 use std::sync::Arc;
 use ntfs::{
@@ -28,14 +26,15 @@ pub fn start<P: AsRef<Path>>(path: P) {
     let iocp = Arc::new(IOCompletionPort::new(1).unwrap());
 
     let mut reader = AsyncReader::new(pool.clone(), iocp.clone(), path, 42);
-    let mut consumer = AsyncConsumer::new(pool.clone(), iocp.clone(), MftParser { volume, count: 0 });
+    let mut parser = MftParser { volume, count: 0, pool: pool.clone(), iocp: iocp.clone() };
+//    let mut consumer = AsyncConsumer::new(pool.clone(), iocp.clone(), parser);
 
     let read_thread = thread::Builder::new().name("producer".to_string()).spawn(move || {
         read_all(&mft, volume, &mut reader);
     }).unwrap();
     let consume_thread = thread::Builder::new().name("consumer".to_string()).spawn(move || {
-        consumer.consume();
-        println!("{}", consumer.consumer.count);
+        parser.parse_record();
+        println!("{}", parser.count);
     }).unwrap();
     read_thread.join().expect("reader panic");
     consume_thread.join().expect("consumer panic");
@@ -44,9 +43,26 @@ pub fn start<P: AsRef<Path>>(path: P) {
 struct MftParser {
     volume: VolumeData,
     count: u64,
+    pool: BufferPool,
+    iocp: Arc<IOCompletionPort>,
 }
 
-impl Consumer for MftParser {
+impl MftParser {
+    pub fn parse_record(&mut self) {
+        let mut connection = sql::main();
+        let tx = connection.transaction().unwrap();
+        loop {
+            let mut operation = self.iocp.get().unwrap();
+            if operation.completion_key() != 42 {
+                break;
+            }
+            self.consume(&mut operation, &tx);
+            self.pool.put(operation.into_buffer());
+        }
+        tx.commit().unwrap();
+
+    }
+
     fn consume(&mut self, operation: &mut OutputOperation, tx: &Transaction) {
         for buff in operation.buffer_mut().chunks_mut(self.volume.bytes_per_file_record as usize) {
             let entry = parse_file_record_basic(buff, self.volume);
--- src/windows/async_io/mod.rs
+++ src/windows/async_io/mod.rs
@@ -1,18 +1,13 @@
 mod iocp;
 mod buffer_pool;
-mod async_producer;
-mod async_consumer;
-pub use self::async_consumer::{
-    AsyncConsumer,
-    Consumer,
-};
+mod async_reader;
 pub use self::buffer_pool::BufferPool;
 pub use self::iocp::{
     IOCompletionPort,
     OutputOperation,
     AsyncFile,
 };
-pub use self::async_producer::{
+pub use self::async_reader::{
     AsyncReader,
 };
 
