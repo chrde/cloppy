3d4fc2efca2a6c290cd33e307c54b30cbe4ded7e
Christian de la Hoz <apigolas@gmail.com>
2018-04-15 11:25:07 +0200

    before rebase

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #![feature(plugin, custom_attribute, test)]
+#![allow(dead_code)]
 #![recursion_limit = "1024"]
+#[macro_use]
+extern crate bitflags;
 extern crate byteorder;
+extern crate conv;
 extern crate core;
 extern crate failure;
 #[macro_use]
@@ -9,12 +13,12 @@ extern crate ini;
 #[macro_use]
 extern crate nom;
 extern crate parking_lot;
-extern crate test;
-extern crate winapi;
-#[macro_use]
-extern crate bitflags;
 extern crate rusqlite;
+extern crate test;
 extern crate time;
+#[macro_use]
+extern crate typed_builder;
+extern crate winapi;
 
 use errors::failure_to_string;
 
@@ -24,7 +28,7 @@ mod sql;
 //mod user_settings;
 mod errors;
 
-fn main() {
+fn ntfs_main() {
     if let Err(e) = ntfs::start() {
         println!("{}", failure_to_string(e));
     }
